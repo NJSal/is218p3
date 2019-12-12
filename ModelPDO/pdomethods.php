2ba5a926790660a6cc348b4afeb62c166225fcb3
David <dfs23@njit.edu>
2019-12-12 12:05:15 -0500

    feat: fixed typo in validate log in

--- ModelPDO/pdomethods.php
+++ ModelPDO/pdomethods.php
@@ -85,7 +85,7 @@ function validate_registration($email, $firstname, $lastname, $birthday, $lastna
 function delete_question($id) {
     global $db;
     $query = 'DELETE FROM questions
-              WHERE ownerid = :id';
+              WHERE id = :id';
     $statement = $db->prepare($query);
     $statement->bindValue(':id', $id);
     $statement->execute();
